94a1b2e2ff7356fef3852ae4e0e1550e7dbf8989
AM_Trustup <axelismv4630@gmail.com>
2023-05-04 14:10:43 +0200

    :bug: Fixed

--- src/runtime/layout/auth-layout.vue
+++ src/runtime/layout/auth-layout.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <slot v-if="!auth.isLoading && auth.user" />
+    <slot
+      v-if="
+        (!auth.isLoading && auth.user) ||
+        route.path == '/trustup-io/auth/callback'
+      "
+    />
     <div v-else>
       <AppLoader />
     </div>
@@ -9,6 +14,8 @@
 
 <script setup lang="ts">
 import { AppLoader } from "@deegital/vue-3-trustup-io-loader";
-import { useAuth } from "#imports";
+import { useAuth, useRoute } from "#imports";
 const auth = useAuth();
+
+const route = useRoute();
 </script>
--- src/runtime/middleware/auth.ts
+++ src/runtime/middleware/auth.ts
@@ -4,7 +4,7 @@ import { useAuth } from "../composables/useAuth";
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const path = window.location.origin + to.fullPath;
 
-  if (from.fullPath === to.fullPath && to.path === "trustup-io/auth/callback")
+  if (from.fullPath === to.fullPath && to.path === "/trustup-io/auth/callback")
     return;
 
   const auth = useAuth();
