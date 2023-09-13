5b6e46ef83c397a0535cbc410c0d30d694b19ebc
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-09-13 14:18:57 +0200

    :bug: Missing environment commons.

---
+++ .changeset/cyan-frogs-attend.md
@@ -0,0 +1,5 @@
+---
+"@deegital/nuxt-trustup-io-auth": patch
+---
+
+Missing environment commons.
--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -2,6 +2,12 @@
 
 true > .env && \
 doppler secrets download \
+  --project "trustup-io-app-commons" \
+  --config local \
+  --no-file \
+  --format env | grep 'NUXT_' \
+  >> .env
+doppler secrets download \
   --project "nuxt-trustup-io-auth" \
   --config local \
   --no-file \
